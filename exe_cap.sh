5aedd1a76909ff6db7ad0f6d5de74f608b281084
Yuta Tokuoka <tokuoka@fun.bio.keio.ac.jp>
2017-06-18 07:25:16 +0900

    Bug fixed exe_cap.py

--- exe_cap.sh
+++ exe_cap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -e LearnedModel.model]; then
+if [ ! -e LearnedModel.model ]; then
     wget https://fun.bio.keio.ac.jp/~tokuoka/Downloads/LearnedModel.model -P .
 fi
 python main_cap.py -m LearnedModel.model
